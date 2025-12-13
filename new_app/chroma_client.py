9637b58b6c0a99bea0c01cdc6863db456c24e828
Thunge Rammohan <thungerammohan@gmail.com>
2025-12-13 15:35:21 +0530

    6commit

--- new_app/chroma_client.py
+++ new_app/chroma_client.py
@@ -1,9 +1,17 @@
+import os
 import chromadb
 
+CHROMA_API_KEY = os.getenv("CHROMA_API_KEY")
+CHROMA_TENANT = os.getenv("CHROMA_TENANT")
+CHROMA_DATABASE = os.getenv("CHROMA_DATABASE")
+
+if not CHROMA_API_KEY or not CHROMA_TENANT or not CHROMA_DATABASE:
+    raise ValueError("Chroma environment variables are missing")
+
 client = chromadb.CloudClient(
-    api_key="ck-6UALWqytpjaiAG7KW4o2dhagVTQgH7VvEhtKGscjqkc8",
-    tenant="5dcdb837-1e31-4764-a4c1-5cf27b6ecef7",
-    database="rag_model"
+    api_key=CHROMA_API_KEY,
+    tenant=CHROMA_TENANT,
+    database=CHROMA_DATABASE
 )
 
 collection = client.get_or_create_collection(
